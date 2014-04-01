6dbcca35ea14b7733ea5ea0466cf9030f4b5eee1
Laura Feier <laura.feier@3pillarglobal.com>
2014-04-01 16:01:21 +0300

    replaced author var name with user. mistake when moving code around.

--- cms_blogger/utils.py
+++ cms_blogger/utils.py
@@ -3,8 +3,8 @@ from django.utils.encoding import smart_unicode
 
 def user_display_name(user):
     if user.first_name and user.last_name:
-        return u'%s %s' % (self.author.first_name, user.last_name)
+        return u'%s %s' % (user.first_name, user.last_name)
     elif user.email:
         return user.email
     else:
-        return smart_unicode(self.author)
+        return smart_unicode(user)
