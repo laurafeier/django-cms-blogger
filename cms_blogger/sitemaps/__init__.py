f5eed7f9938a7d46cb9db81dfdfba03b70f68ad6
Zoltan Szeredi <zoltan.szeredi@3pillarglobal.com>
2014-04-01 17:48:20 +0300

    Add support for sitemaps.

---
+++ cms_blogger/sitemaps/__init__.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from django.contrib.sitemaps import Sitemap
+from django.contrib.sites.models import Site
+from django.core import paginator
+from django.db.models import Q
+from cms_blogger.models import (
+    BlogEntryPage,
+    BioPage,
+    Blog,
+)
+import itertools
+
+
+class BloggerSitemap(Sitemap):
+    changefreq = "monthly"
+    priority = 0.5
+
+    def items(self):
+        # Blogs, BlogRelatedPages, BlogEntryPages
+        current_site = Site.objects.get_current()
+        blog_current_site = Q(blog__site=current_site)
+        blogs = Blog.objects.filter(site=current_site)
+        bio_pages = BioPage.objects.filter(blog_current_site)
+        entry_pages = BlogEntryPage.objects.filter(blog_current_site)
+        chained = itertools.chain(blogs, bio_pages, entry_pages)
+        return list(chained)
+
+    def lastmod(self, page):
+        import datetime
+        return datetime.datetime.now() + datetime.timedelta(days=6)
+
+
