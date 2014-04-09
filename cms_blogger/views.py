c064adb2565dcd011e3093d6ec7d8b6d0bb9dfbf
Laura Feier <laura.feier@3pillarglobal.com>
2014-04-09 17:59:23 +0300

    Added search functionality for landing page view.

--- cms_blogger/views.py
+++ cms_blogger/views.py
@@ -2,9 +2,11 @@ from django.http import HttpResponse, Http404, HttpResponseNotFound
 from django.shortcuts import get_object_or_404
 from django.contrib.sites.models import Site
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.db.models import Q
 from cms_layouts.layout_response import LayoutResponse
 from .models import BlogEntryPage, Blog
 from .settings import POSTS_ON_LANDING_PAGE
+import re
 
 
 def get_entries_queryset_for_request(request):
@@ -29,6 +31,26 @@ def entry_page(request, blog_slug, year, month, day, entry_slug):
     return entry.render_to_response(request)
 
 
+def get_terms(query_string):
+    query_string = query_string.strip()
+    # remove quotes
+    query_string = re.sub('\'|\"', '', query_string)
+    # strip multiple spaces
+    query_string = re.sub('\s{2,}', ' ', query_string)
+    return query_string.split()
+
+
+def get_query(query_string, search_fields):
+    terms = get_terms(query_string)
+    query = Q()
+    for term in terms:
+        term_query = Q()
+        for field_name in search_fields:
+            term_query |= Q(**{"%s__icontains" % field_name: term})
+        query &= term_query
+    return query
+
+
 def landing_page(request, blog_slug):
     blog = get_object_or_404(
         Blog, slug=blog_slug, site=Site.objects.get_current())
@@ -37,8 +59,15 @@ def landing_page(request, blog_slug):
         return HttpResponseNotFound(
             "<h1>This Blog Landing Page does not have a "
             "layout to render.</h1>")
+    entries = blog.get_entries()
+    search_q = request.GET.get('q')
+    extra_params = ''
+    if search_q and search_q.strip():
+        extra_params = "&q=%s" % search_q.strip()
+        search_fields = ('title', 'short_description')
+        entries = entries.filter(get_query(search_q, search_fields))
 
-    paginator = Paginator(blog.get_entries(), POSTS_ON_LANDING_PAGE)
+    paginator = Paginator(entries, POSTS_ON_LANDING_PAGE)
     page = request.GET.get('page')
     try:
         entries = paginator.page(page)
@@ -48,6 +77,7 @@ def landing_page(request, blog_slug):
     except EmptyPage:
         # If page is out of range (e.g. 9999), deliver last page of results.
         entries = paginator.page(paginator.num_pages)
+    setattr(entries, 'extra_params', extra_params)
     blog.paginated_entries = entries
     return LayoutResponse(blog, layout, request).make_response()
 
