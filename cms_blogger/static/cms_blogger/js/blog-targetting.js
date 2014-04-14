12632c9e7d9831dad688369a731c36930a27676d
Alex Pirjol <alexandru.pirjol@3pillarglobal.com>
2014-04-14 17:34:40 +0300

    LUN-1507 : Change namespace to 'cms-blogger'

--- cms_blogger/static/cms_blogger/js/blog-targetting.js
+++ cms_blogger/static/cms_blogger/js/blog-targetting.js
@@ -1,6 +1,6 @@
 (function(){
     //jQuery is not available yet, use vanilla js
-    if(document.body.className.indexOf('pbs-blogger') < 0){
-        document.body.className += " pbs-blogger";
+    if(document.body.className.indexOf('cms-blogger') < 0){
+        document.body.className += " cms-blogger";
     }
 })();
