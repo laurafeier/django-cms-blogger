a7e666163691b592ee0e5a166e1bc370caf5df5c
Titus Nicolae <nicolae.titus@3pillarglobal.com>
2014-04-25 11:19:37 +0300

    style changes

--- cms_blogger/static/cms_blogger/js/entry-preview.js
+++ cms_blogger/static/cms_blogger/js/entry-preview.js
@@ -1,24 +1,18 @@
-
 function showEntryPreviewPopup(triggeringLink) {
-    
     $.ajax({
-           type:  "POST",
-       datatype:  "text",
-            url:  triggeringLink.href,
-           data:  {"body": tinyMCE.activeEditor.getContent()},
-        success:  function(data) {
-            var win = window.open('', 'about:blank', 'height=800,width=1024,resizable=yes,scrollbars=yes'); 
-            if (win && win.document)
-            {
-                with(win.document)
-                {
+        type:  "POST",
+        datatype:  "text",
+        url:  triggeringLink.href,
+        data:  { "body": tinyMCE.activeEditor.getContent() },
+        success: function(data) {
+            var win = window.open('', 'about:blank', 'height=800,width=1024,resizable=yes,scrollbars=yes');
+            if (win && win.document) {
+                with(win.document) {
                     open();
-                    write(data); 
+                    write(data);
                     close();
-                } 
-            }
-            else
-            {
+                }
+            } else {
                 alert('Your popup blocker is preventing the Preview Page from opening.');
             }
         }
