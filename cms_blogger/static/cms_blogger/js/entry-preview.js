50ab8354418ebad6914cabafc0278babbf6c1e18
Titus Nicolae <nicolae.titus@3pillarglobal.com>
2014-04-25 15:51:30 +0300

    open window on click instead of ajax on success

--- cms_blogger/static/cms_blogger/js/entry-preview.js
+++ cms_blogger/static/cms_blogger/js/entry-preview.js
@@ -1,19 +1,15 @@
 function showEntryPreviewPopup(triggeringLink) {
+    var win = window.open('', 'entry-preview', 'height=800,width=1024,resizable=yes,scrollbars=yes');
     $.ajax({
         type:  "POST",
         datatype:  "text",
         url:  triggeringLink.href,
         data:  { "body": tinyMCE.activeEditor.getContent() },
         success: function(data) {
-            var win = window.open('', 'entry-preview', 'height=800,width=1024,resizable=yes,scrollbars=yes');
-            if (win && win.document) {
-                with(win.document) {
-                    open();
-                    write(data);
-                    close();
-                }
-            } else {
-                alert('Your popup blocker is preventing the Preview Page from opening.');
+            with(win.document) {
+                open();
+                write(data);
+                close();
             }
         }
     });
